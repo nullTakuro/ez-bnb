fbea74956f75a1567dd1ccd6589eb04525980913
nullTakuro <f-op@hotmail.co.uk>
2017-03-07 19:20:27 +0000

    Database setup 1/2

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+# Each url is linked to a views return
 urlpatterns = [
+    #/login/
     url(r'^$', views.index, name='index'),   #difault home page for the login page
+    #https://www.youtube.com/watch?v=mWofrhTwGWQ
 ]
