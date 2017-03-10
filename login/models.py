9ccd7ab06c157beaf6b3946ab8a520863b234796
nullTakuro <f-op@hotmail.co.uk>
2017-03-10 20:45:46 +0000

    3/4th database commit

--- login/models.py
+++ login/models.py
@@ -5,14 +5,43 @@ from django.db import models
 # and then this = python manage.py migrate
 
 class User(models.Model):
-    user = models.CharField(max_length=20)
+    # userID is auto Generated
+    username = models.CharField(editable=False, max_length=20)
     firstName = models.CharField(max_length=20)
     lastName = models.CharField(max_length=20)
-    profilePic = models.CharField(max_length=1000)
+    email = models.EmailField(max_length=254)       # A CharField that checks that the value is a valid email address. It uses EmailValidator to validate the input.
+    profilePic = models.URLField(max_length=1000)       # A CharField for a URL.
 
     def __str__(self):
-        return self.user
+        return self.username        # Returns the specific value as the value of the username
 
 class Property(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)        #uses the primary key asssigned to each user and imports it as a ForeignKey into the Property table. When a user is deleted the Propert associated is also deleted!
-    #INSERT MORE COLUMNS!
+    # propertyID is auto Generated
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)        #uses the primary key asssigned to each user and imports it as a ForeignKey into the Property table. When a user is deleted the Propert associated is also deleted!
+    assignedName = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.assignedName        # Returns the specific value as the value of the assignedName
+
+class Address(models.Model):
+    # addressID is auto Generated
+    propertyID = models.ForeignKey(Property, on_delete=models.CASCADE)        # A many-to-one relationship. Requires a positional argument: the class to which the model is related.
+    LineOne = models.CharField(max_length=50)
+    LineTwo = models.CharField(max_length=50)
+    City = models.CharField(max_length=50)
+    Country = models.CharField(max_length=50)
+
+class Booking(models.Model):
+    # bookingID is auto Generated
+    propertyID = models.ForeignKey(Property, on_delete=models.CASCADE)      # A many-to-one relationship. Requires a positional argument: the class to which the model is related.
+    startDateTime = models.DateTimeField(auto_now=False, auto_now_add=False)        # A date and time, represented in Python by a datetime.datetime instance
+    duration = models.DurationField()       # A field for storing periods of time - modeled in Python by timedelta. When used on PostgreSQL, the data type used is an interval and on Oracle the data type is INTERVAL DAY(9) TO SECOND(6).
+    endDateTime = models.DateTimeField(auto_now=False, auto_now_add=False)        # A date and time, represented in Python by a datetime.datetime instance
+
+class Guest(models.Model):
+    # guestID is auto Generated
+    bookingID = models.ForeignKey(Bookings, on_delete=models.CASCADE)       # A many-to-one relationship. Requires a positional argument: the class to which the model is related.
+    gFirstName = models.CharField(max_length=20)
+    gLastName = models.CharField(max_length=20)
+    gPhone = models.BigIntegerField(max_length=14)      # A 64-bit integer, much like an IntegerField except that it is guaranteed to fit numbers from -9223372036854775808 to 9223372036854775807.
+    gEmail = models.EmailField(max_length=254)       # A CharField that checks that the value is a valid email address. It uses EmailValidator to validate the input.
--- login/urls.py
+++ login/urls.py
@@ -4,6 +4,5 @@ from . import views
 # Each url is linked to a views return
 urlpatterns = [
     #/login/
-    url(r'^$', views.index, name='index'),   #difault home page for the login page
-    #https://www.youtube.com/watch?v=mWofrhTwGWQ
+    url(r'^$', views.index, name='index'),   #default home page for the login page
 ]
