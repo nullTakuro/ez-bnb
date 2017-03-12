2246e8935646aa2c41aad78aebd660b7a106b990
nullTakuro <f-op@hotmail.co.uk>
2017-03-12 20:44:45 +0000

    fixed all issues :)

--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import user
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1> This is the login page!</h1>")
+    all_users = user.objects.all()
+    template = loader.get_template('main/index.html')
+    context = {                         # Setting up a dictionary.
+        'all_users' : all_users,
+    }
+    return HttpResponse(template.render(context, request))
 
 def bookingRender(request, bookingID):
     return HttpResponse("<h1> Booking Render for " + str(bookingID) + "</h1>")
